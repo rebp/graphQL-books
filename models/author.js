1fabc363b6414ee8b7798a53e4915a9597dd743f
Rober <rober.prieto@fontoxml.com>
2019-10-14 16:47:38 +0200

    Extend author model

--- models/author.js
+++ models/author.js
@@ -1,9 +1,20 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const validator = require('validator');
 const Schema = mongoose.Schema;
 
 const authorSchema = new Schema({
-  name: String,
-  age: Number
+	name: { type: String, required: true },
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		lowercase: true,
+		validate: value => {
+			return validator.isEmail(value);
+		}
+	},
+	age: { type: String, required: false },
+	gender: { type: String, required: false }
 });
 
-module.exports = mongoose.model("Author", authorSchema);
+module.exports = mongoose.model('Author', authorSchema);
--- schema/schema.js
+++ schema/schema.js
@@ -3,13 +3,14 @@ const Book = require('../models/book');
 const Author = require('../models/Author');
 
 const {
-	GraphQLObjectType,
-	GraphQLString,
-	GraphQLSchema,
+	GraphQLEnumType,
 	GraphQLID,
 	GraphQLInt,
 	GraphQLList,
-	GraphQLNonNull
+	GraphQLNonNull,
+	GraphQLObjectType,
+	GraphQLSchema,
+	GraphQLString
 } = graphql;
 
 const BookType = new GraphQLObjectType({
@@ -27,12 +28,22 @@ const BookType = new GraphQLObjectType({
 	})
 });
 
+var genderType = new GraphQLEnumType({
+	name: 'Gender',
+	values: {
+		man: { value: 'man' },
+		woman: { value: 'woman' }
+	}
+});
+
 const AuthorType = new GraphQLObjectType({
 	name: 'Author',
 	fields: () => ({
 		id: { type: GraphQLID },
 		name: { type: GraphQLString },
+		email: { type: GraphQLString },
 		age: { type: GraphQLInt },
+		gender: { type: GraphQLString },
 		books: {
 			type: new GraphQLList(BookType),
 			resolve(parent, args) {
@@ -94,17 +105,28 @@ const authorMutations = {
 		args: {
 			id: { type: new GraphQLNonNull(GraphQLID) },
 			name: { type: GraphQLString },
-			age: { type: GraphQLInt }
+			email: { type: GraphQLString },
+			age: { type: GraphQLInt },
+			gender: { type: genderType }
 		},
 		async resolve(parent, args) {
 			const author = await Author.findById(args.id);
 
 			const updatedAuthor = {
 				name: !args.name ? author.name : args.name,
-				age: !args.age ? author.age : args.age
+				email: !args.email ? author.email : args.email,
+				age: !args.age ? author.age : args.age,
+				gender: !args.gender ? author.gender : args.gender
 			};
 
 			return Author.findByIdAndUpdate(args.id, updatedAuthor, { new: true });
+		}
+	},
+	deleteAuthor: {
+		type: AuthorType,
+		args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+		resolve(parent, args) {
+			return Author.findOneAndDelete(args.id);
 		}
 	}
 };
@@ -124,6 +146,31 @@ const bookMutations = {
 				authorId: args.authorId
 			});
 			return book.save();
+		}
+	},
+	editBook: {
+		type: BookType,
+		args: {
+			id: { type: new GraphQLNonNull(GraphQLID) },
+			name: { type: GraphQLString },
+			age: { type: GraphQLInt }
+		},
+		async resolve(parent, args) {
+			const book = await Book.findById(args.id);
+
+			const updatedBook = {
+				name: !args.name ? book.name : args.name,
+				age: !args.age ? book.age : args.age
+			};
+
+			return Book.findByIdAndUpdate(args.id, updatedBook, { new: true });
+		}
+	},
+	deleteBook: {
+		type: BookType,
+		args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+		resolve(parent, args) {
+			return Book.findOneAndDelete(args.id);
 		}
 	}
 };
